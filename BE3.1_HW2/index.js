5d925455ec4bec57e0e75de14cd26b1981e57ecd
Rohit Bhise <rohitbhise9372@gmail.com>
2026-08-18 16:54:10 +0530

    Add files via upload

---
+++ BE3.1_HW2/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from express server.");
+});
+
+
+app.get("/signin", (req, res) => {
+  res.send("This is the Sign In page.");
+});
+
+
+app.get("/booking", (req, res) => {
+  res.send("Book your tickets here.");
+});
+
+
+app.get("/clothing/kids", (req, res) => {
+  res.send("This is the kids wear page.");
+});
+
+
+app.get("/blog", (req, res) => {
+  res.send("This is the blog page.");
+});
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
