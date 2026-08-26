d24d33235a6ecfbb04be82ad301382d2f026c4ef
Rohit Bhise <rohitbhise9372@gmail.com>
2026-08-26 12:33:09 +0530

    Add files via upload

---
+++ BE4.2_CW/index.js
@@ -0,0 +1,148 @@
+const express = require("express")
+const app = express()
+const { initializeDatabase } = require("./db/db.connect");
+
+// const movie = require("./models/movie.models");
+const Movie = require("./models/movie.models");
+initializeDatabase();
+
+app.use(express.json())
+
+// const newMovie = {
+//   title: "Dilwale Dulhania Le Jayenge",
+//   releaseYear: 1995,
+//   genre: ["Drama"],
+//   director: "Aditya Roy Chopra",
+//   actors: ["Actor1", "Actor2"],
+//   language: "Hindi",
+//   country: "India",
+//   rating: 6.1,
+//   plot: "A young man and woman fall in love on a Australia trip.",
+//   awards: "IFA Filmfare Awards",
+//   posterUrl: "https://example.com/new-poster1.jpg",
+//   trailerUrl: "https://example.com/new-trailer1.mp4",
+// };
+
+async function createMovie(newMovie) {
+  try {
+    const movie = new Movie(newMovie);
+    const saveMovie = await movie.save();
+    return saveMovie
+  } catch (error) {
+    throw error;
+  }
+}
+
+
+app.post("/movies", async (req, res) => {
+  try {
+    const savedMovie = await createMovie(req.body)
+    res.status(201).json({message: "Movie added successfully.", movie: savedMovie })
+  } catch (error) {
+    res.status(500).json({error: "Failed to add movie"})
+  }
+})
+
+// createMovie(newMovie)
+
+
+// find a movie with a particular title
+async function readMovieByTitle(movieTitle) {
+  try {
+    const movie = await Movie.findOne({ title: movieTitle })
+    return movie
+  } catch (error) {
+    throw error
+  }
+}
+
+app.get("/movies/:title",async (req, res) => {
+  try {
+    const movie = await readMovieByTitle(req.params.title)
+    if (movie) {
+      res.json(movie)
+    } else {
+      res.status(404).json({error: "Movie not found."})
+    }
+  } catch (error) {
+    res.status(500).json({error: "Failed to fetch movie."})
+   }
+})
+
+// to get all the movies in the database
+
+async function readAllMovies() {
+  try {
+    const allMovies = await Movie.find()
+    return allMovies
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+app.get("/movies",async (req, res) => {
+  try {
+    const movies = await readAllMovies(req, res)
+    if (movies.length != 0) {
+      res.json(movies)
+    } else {
+      res.status(404).json({error: "No movies found."})
+    }
+  } catch (error) {
+    res.status(500).json({error: "Failed to fetch movies."})
+   }
+})
+
+
+// get movie by director name
+async function readMovieByDirector(directorName) {
+  try {
+    const movieByDirector = await Movie.find({ director: directorName })
+    return movieByDirector
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+app.get("/movies/director/:directorName",async (req, res) => {
+  try {
+    const movies = await readMovieByDirector(req.params.directorName)
+    if (movies.length != 0) {
+      res.json(movies)
+    } else {
+      res.status(404).json({error: "No movies found."})
+    }
+  } catch (error) {
+    res.status(500).json({error: "Failed to fetch movies."})
+   }
+})
+
+
+async function readMovieByGenre(genreNAme) {
+  try {
+    const movieByGenre = await Movie.find({ genre: genreNAme })
+    return movieByGenre
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+app.get("/movies/genres/:genreName",async (req, res) => {
+  try {
+    const movies = await readMovieByGenre(req.params.genreName)
+    if (movies.length != 0) {
+      res.json(movies)
+    } else {
+      res.status(404).json({error: "No movies found."})
+    }
+  } catch (error) {
+    res.status(500).json({error: "Failed to fetch movies."})
+   }
+})
+
+
+const PORT = 3000
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`)
+})
