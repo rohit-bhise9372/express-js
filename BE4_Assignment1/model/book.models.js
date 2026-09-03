06012511fc3435d4bea1da31d51f2036385159c3
Rohit Bhise <rohitbhise9372@gmail.com>
2026-09-03 07:27:46 +0530

    Add files via upload

---
+++ BE4_Assignment1/model/book.models.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  publishedYear: Number,
+  genre: [String],
+  language: String,
+  country: String,
+  rating: Number,
+  summary: String,
+  coverImageUrl: String,
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
